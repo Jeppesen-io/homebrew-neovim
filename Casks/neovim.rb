80f53d1561e2a59e7a96b493e1adb15c7d612c19
Nelson Jeppesen <nelson@nelsonjeppeson-mbp.local>
2016-05-12 10:48:43 -0700

    Update artifact

--- Casks/neovim.rb
+++ Casks/neovim.rb
@@ -1,5 +1,5 @@
 cask 'neovim' do
-  version '0.3'
+  version '0.4'
   url 'https://github.com/Jeppesen-io/homebrew-bash/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-bash'
   sha256 :no_check
@@ -12,7 +12,7 @@ cask 'neovim' do
   depends_on formula: 'wget'
   depends_on formula: 'python3'
 
-  artifact 'homebrew-neovom-master/dot-files', target: "#{ENV['HOME']}/.config/nvim"
+  artifact 'homebrew-neovom-master/dot-files/init.vim', target: "#{ENV['HOME']}/.config/nvim/init.vim"
 
 postflight do
 
