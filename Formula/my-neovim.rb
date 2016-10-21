b9e1f332139adac85828cc83b216c330e5805572
Nelson Jeppesen <nelson@simplymeasured.com>
2016-10-21 14:57:42 -0700

    remove Cask in favor of a Formula

--- Formula/my-neovim.rb
+++ Formula/my-neovim.rb
@@ -2,32 +2,37 @@ class MyNeovim < Formula
   desc 'Install Neovim how I like it'
   url 'https://github.com/Jeppesen-io/homebrew-neovim/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-neovim'
-  version '0.5'
+  version '1.0.1'
 
-  depends_on 'neovim/neovim/neovim' => :HEAD
+  depends_on 'neovim/neovim/neovim'
   depends_on 'curl'
   depends_on 'python3'
 
   bottle :unneeded
 
   def install
-    # No action
-  end
 
-  def post_install
+    # Get home dir (stripped by homebrew)
+    home_dir = `sudo -Hu $USER bash -c 'echo -n $HOME'`
+
+    # Create xdg dirs for neovim
+    system 'mkdir', '-vp', "#{home_dir}/.config/nvim/"
+    system 'mkdir', '-vp', "#{home_dir}/.cache/nvim/"
+    system 'mkdir', '-vp', "#{home_dir}/.local/nvim/"
 
     # Copy over my configuration
-    `mkdir -p ~/.config/nvim/`
-    `cp -v dot-files/* ~/.config/nvim/`
+    system 'cp', '-v', './dot-files/init.vim'	, "#{prefix}"
+    system 'ln', '-s', "#{prefix}/init.vim"	, "#{home_dir}/.config/nvim/init.vim"
 
     # Install vim-plug, a minimalist Vim plugin manager
-    `curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
+    system 'curl', '-fLo', "#{prefix}/plug.vim", 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    system 'ln', '-s', "#{prefix}/plug.vim", "#{home_dir}/.config/nvim/plug.vim"
 
     # Link nvim to vim
-    `ln -svf /usr/local/bin/nvim /usr/local/bin/vim`
+    system 'ln', '-svf', '/usr/local/bin/nvim', '/usr/local/bin/vim'
 
     # Install latest neovom python3 bindings for deoplete plugin
-    `pip3 install neovim --upgrade`
+    system 'pip3', 'install', 'neovim', '--upgrade'
 
   end
 
