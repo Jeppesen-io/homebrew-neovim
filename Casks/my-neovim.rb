57f958aba61261d1c8b34d2727650f53cbbc90fd
Nelson Jeppesen <nelson@simplymeasured.com>
2017-09-05 10:37:54 -0700

    Bring in plugin tool

--- Casks/my-neovim.rb
+++ Casks/my-neovim.rb
@@ -1,11 +1,12 @@
 cask 'my-neovim' do
 
-  version '1.0.1'
+  version '1.0.2'
   url 'https://github.com/Jeppesen-io/homebrew-neovim/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-neovim'
   sha256 :no_check
 
   artifact 'homebrew-neovim-master/dot-files/init.vim',   target: "#{ENV['HOME']}/.config/nvim/init.vim"
+  artifact 'homebrew-neovim-master/dot-files/plug.vim',   target: "#{ENV['HOME']}/.config/nvim/plug.vim"
 
   depends_on formula: 'neovim'
   depends_on formula: 'curl'
@@ -18,10 +19,10 @@ cask 'my-neovim' do
     `mkdir -vp #{ENV['HOME']}/.local/nvim`
 
     `rm -f #{ENV['HOME']}/.config/nvim/init.vim`
+    `rm -f #{ENV['HOME']}/.config/nvim/plug.vim`
   end
 
   postflight do
-    `curl -fLo #{ENV['HOME']}/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
 
     `ln -svf /usr/local/bin/nvim /usr/local/bin/vim`
 
