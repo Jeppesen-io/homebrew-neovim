99f3905299c75c9c9e54b0207911c0819e101296
Nelson Jeppesen <Nelson>
2017-09-05 15:28:28 -0700

    Bump version

--- Casks/my-neovim.rb
+++ Casks/my-neovim.rb
@@ -1,6 +1,6 @@
 cask 'my-neovim' do
 
-  version '1.0.4'
+  version '1.0.5'
   url 'https://github.com/Jeppesen-io/homebrew-neovim/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-neovim'
   sha256 :no_check
