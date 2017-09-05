dcc141706bc0b956539003ae5454b44f74b2c77b
Nelson Jeppesen <Nelson>
2017-09-05 15:13:57 -0700

    Remove plug

--- Casks/my-neovim.rb
+++ Casks/my-neovim.rb
@@ -1,12 +1,11 @@
 cask 'my-neovim' do
 
-  version '1.0.2'
+  version '1.0.3'
   url 'https://github.com/Jeppesen-io/homebrew-neovim/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-neovim'
   sha256 :no_check
 
   artifact 'homebrew-neovim-master/dot-files/init.vim',   target: "#{ENV['HOME']}/.config/nvim/init.vim"
-  artifact 'homebrew-neovim-master/dot-files/plug.vim',   target: "#{ENV['HOME']}/.config/nvim/plug.vim"
 
   depends_on formula: 'neovim'
   depends_on formula: 'curl'
@@ -19,7 +18,6 @@ cask 'my-neovim' do
     `mkdir -vp #{ENV['HOME']}/.local/nvim`
 
     `rm -f #{ENV['HOME']}/.config/nvim/init.vim`
-    `rm -f #{ENV['HOME']}/.config/nvim/plug.vim`
   end
 
   postflight do
